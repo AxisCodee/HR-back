51ec1e8c25ea656c0cd3e2f764903edef9c2c707
AxisCodee <raneemma934@gmail.com>
2024-01-07 12:36:49 +0300

    Commit message

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\API;
+use TADPHP\TADFactory;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+require 'tad/vendor/autoload.php';
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+        $token = Auth::attempt($credentials);
+
+        if (!$token) {
+            return response()->json([
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+
+        $tad_factory = new TADFactory(['ip'=>'192.168.2.202']);
+$tad = $tad_factory->get_instance();
+$r = $tad->set_user_info([
+    'pin' => $request->pin,//this is the pin2 in the returned response
+    'name'=> $request->first_name,
+    'privilege'=> 0,//if you want to add a superadmin user make the privilege as '14'.
+    'password' => $request->password]);
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'role_id'=> $request->role_id,
+            'department_id' => $request->department_id,
+            'password' => Hash::make($request->password),
+
+        ]);
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user
+        ]);
+
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    public function refresh()
+    {
+        return response()->json([
+            'user' => Auth::user(),
+            'authorisation' => [
+                'token' => Auth::refresh(),
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+}
---
+++ app/Http/Controllers/AttendanceController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use TADPHP\TAD;
+use TADPHP\TADFactory;
+require 'tad/vendor/autoload.php';
+
+class AttendanceController extends Controller
+{
+    public function getAttendanceLogs()
+    {
+        $tad_factory = new TADFactory(['ip' => '192.168.2.202']);
+        $tad = $tad_factory->get_instance();
+
+        $all_user_info = $tad->get_all_user_info();
+        $dt = $tad->get_date();
+        $logs = $tad->get_att_log();
+
+        $xml = simplexml_load_string($logs);
+        $array = json_decode(json_encode($xml));
+        $json_output = json_encode($array);
+
+    echo $json_output;
+    }
+
+
+    
+
+
+
+
+    //_____________________add new normal user__________________//
+// $tad_factory = new TADFactory(['ip'=>'192.168.2.202']);
+// $tad = $tad_factory->get_instance();
+// $r = $tad->set_user_info([
+//     'pin' => USER_PIN2,//this is the pin2 in the returned response
+//     'name'=> 'testing user',
+//     'privilege'=> 0,//if you want to add a superadmin user make the privilege as '14'.
+//     'password' => ''
+// ]);
+}
+
