e12ba7ef3d6bfffa6414f4e377ebc09b58fe17c6
ali007dev <ali007developer@gmail.com>
2024-01-17 10:40:19 +0300

    aa

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -71,13 +71,13 @@ class AttendanceController extends Controller
         ]);
     }
 
-    public function showAttendanceUser(User $user){
+    public function showAttendanceUser($user){
 
         $result=User::with('attendance')->get();
 
-        return  ResponseHelper::success([
+        return  ResponseHelper::success(
             $result
-        ]);
+        );
     }
 
 }
