eeba6006de2be355b29e06e65d920f025ff3d756
ali007dev <ali007developer@gmail.com>
2024-01-17 16:40:04 +0300

    aa

--- app/Models/User.php
+++ app/Models/User.php
@@ -89,7 +89,7 @@ class User extends Authenticatable implements JWTSubject
     }
     public function roles()
     {
-        return $this->belongsToMany(Role::class);
+        return $this->belongsToMany(Role::class,'user_id');
     }
 
     public function my_decisions()
