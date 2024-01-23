66f6e62ddcc06967c4ff11c840ac127db13e45d8
mazhar <mazharalhayek12@gmail.com>
2024-01-23 13:52:50 +0300

    register section

--- database/migrations/2024_01_22_181611_create_additional_files_table.php
+++ database/migrations/2024_01_22_181611_create_additional_files_table.php
@@ -4,16 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('careers', function (Blueprint $table) {
+        Schema::create('additional_files', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->string('content');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('description');
+            $table->string('path');
             $table->timestamps();
         });
     }
@@ -23,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('careers');
+        Schema::dropIfExists('additional_files');
     }
 };
