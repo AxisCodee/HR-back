ba8b31a42de19a8e517d5ccd48a8268855d9d903
ali007dev <ali007developer@gmail.com>
2024-01-26 23:30:11 +0300

    aa

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -63,21 +63,25 @@ class AttendanceController extends Controller
 
             // the first of check the late
             $checkInDate = substr($log['DateTime'], 0, 10);
-            $checkInHour = substr($log['DateTime'], 11, 15);
+            $checkInHour=substr($log['DateTime'], 11, 15);
             $checkOutHour = substr($log['DateTime'], 11, 15);
 
+
             $parsedHour = Carbon::parse($checkInHour);
-            $parsedHourOut = Carbon::parse($checkOutHour);
+            $parsedHourOut= Carbon::parse($checkOutHour);
             $companyStartTime = '09:30';
             $companyEndTime = '17:00';
 
-            //check if the person is late
-            if ($parsedHour->isAfter($companyStartTime) && $log['Status'] == 0) {
+            //check if the persone late
+
+
+            if (($parsedHour->isAfter($companyStartTime) && $log['Status'] == 0)||
+            ($parsedHour->isAfter($companyEndTime) && $log['Status'] == 1)) {
                 if ($log['Status'] == 1) {
                     $checkOutHour = substr($log['DateTime'], 11, 15);
                 }
                 if ($log['Status'] == 0) {
-                    $checkInHour = $parsedHourOut->format('H:i');
+                    $checkInHour=substr($log['DateTime'], 11, 15);
                 }
 
                 $hoursLate = $parsedHour->diffInHours($companyStartTime);
@@ -94,7 +98,7 @@ class AttendanceController extends Controller
                     $newLateData = [
                         'user_id' => $userId,
                         'lateDate' => $checkInDate,
-                        'check_in' => $log['Status'] == 0 ? $checkInHour : null,
+                        'check_in' => $log['Status'] == 0 ? $checkInHour : null ,
                         'check_out' => $log['Status'] == 1 ? $checkOutHour : null,
                         'hours_num' => $hoursLate
                     ];
