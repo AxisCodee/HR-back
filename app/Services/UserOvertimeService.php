cf471ec171474052460e4aadc1c5f31f9d27f28c
ali007dev <ali007developer@gmail.com>
2024-01-29 21:46:45 +0300

    aa

---
+++ app/Services/UserOvertimeService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Late;
+
+class UserOvertimeService
+{
+    public function checkOvertimeDate($lates, $date)
+    {
+        if ($date) {
+            $year = substr($date, 0, 4);
+            $month = substr($date, 5, 2);
+            $day = substr($date, 8, 2);
+
+            if ($day) {
+                $lates->whereDate('lateDate', $date);
+            } elseif ($month) {
+                $lates->whereYear('lateDate', $year)
+                    ->whereMonth('lateDate', $month);
+            } else {
+                $lates->whereYear('lateDate', $year);
+            }
+        }
+
+        return $lates;
+    }
+}
