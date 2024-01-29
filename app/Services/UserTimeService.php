1d32570f67d3c5f29e29d04ba0a33060809e277c
ali007dev <ali007developer@gmail.com>
2024-01-29 22:32:18 +0300

    aa

--- app/Services/UserTimeService.php
+++ app/Services/UserTimeService.php
@@ -25,4 +25,24 @@ class UsertimeService
 
         return $lates;
     }
+
+    public function checkTimeDate($lates, $date)
+    {
+        if ($date) {
+            $year = substr($date, 0, 4);
+            $month = substr($date, 5, 2);
+            $day = substr($date, 8, 2);
+
+            if ($day) {
+                $lates->whereDate('dateTime', $date);
+            } elseif ($month) {
+                $lates->whereYear('dateTime', $year)
+                    ->whereMonth('dateTime', $month);
+            } else {
+                $lates->whereYear('dateTime', $year);
+            }
+        }
+
+        return $lates;
+    }
 }
