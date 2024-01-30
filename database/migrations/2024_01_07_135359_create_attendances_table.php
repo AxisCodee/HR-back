da606767ef6d271e72e01eca1c9dfdc2980956d8
ali007dev <ali007developer@gmail.com>
2024-01-30 13:11:13 +0300

    شش

--- database/migrations/2024_01_07_135359_create_attendances_table.php
+++ database/migrations/2024_01_07_135359_create_attendances_table.php
@@ -15,22 +15,16 @@ return new class extends Migration
         Schema::create('attendances', function (Blueprint $table) {
             $table->id();
             $table->integer('pin');
-            $table->timestamp('datetime');
+            $table->date('datetime');
             $table->string('verified', 255); // Specify appropriate length
             $table->string('status', 255);   // Specify appropriate length
             $table->string('work_code');
             $table->timestamps();
 
-            // Create a unique constraint for pin, datetime, and status columns
-            $table->unique(['pin', 'datetime', 'status'], 'unique_attendances_pin_datetime_status');
 
-            // Add a generated column for the date part of the datetime column
-            $table->date('date_part')->generatedAs('DATE(`datetime`)')->stored();
-
-            // Index for the generated column
-            $table->index(['date_part'], 'index_attendances_date');
         });
     }
+
 
 
     /**
