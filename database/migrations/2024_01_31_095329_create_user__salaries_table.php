a23cf39d26f0348d90da58e5e762f693392d7042
ali007dev <ali007developer@gmail.com>
2024-02-01 14:32:41 +0300

    aa

--- database/migrations/2024_01_31_095329_create_user__salaries_table.php
+++ database/migrations/2024_01_31_095329_create_user__salaries_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('user__salaries', function (Blueprint $table) {
             $table->id();
-            $table->date('date');
+            $table->string('date');
             $table->double('salary');
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
