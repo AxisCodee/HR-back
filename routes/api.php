8d1ac394b86de76305836f06c15edb687d578852
ali007dev <ali007developer@gmail.com>
2024-02-06 10:50:38 +0300

    aa

--- routes/api.php
+++ routes/api.php
@@ -278,6 +278,8 @@ Route::prefix('Rate')->group(function () {
     ->group(function () {
         Route::post('setRate', 'setRate');
         Route::get('getRate/{id}', 'getRate');
+        Route::get('allRates/{date}', 'allRates');
+
 
 
     });
