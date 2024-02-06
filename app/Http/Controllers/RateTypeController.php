8f331ef76bce9f43509c65ebb2dc7ce551fd8575
ali007dev <ali007developer@gmail.com>
2024-02-06 14:20:06 +0300

    a

--- app/Http/Controllers/RateTypeController.php
+++ app/Http/Controllers/RateTypeController.php
@@ -35,7 +35,7 @@ class RateTypeController extends Controller
     public function getRateType($id)
     {
         try {
-            $result = RateType::where('id', $id)->get()->toArray();
+            $result = RateType::where('branch_id', $id)->get()->toArray();
             return ResponseHelper::success($result, null, 'rateType', 200);
         } catch (\Exception $e) {
             return ResponseHelper::error($e->getMessage(), $e->getCode());
