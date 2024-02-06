4d0c42388860cc924669657f13db9b717241d395
ali007dev <ali007developer@gmail.com>
2024-02-06 11:52:18 +0300

    a

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -109,26 +109,31 @@ class RateController extends Controller
     }
 
     public function allRates(Request $request)
-    {
-        $rates = Rate::with(['rateType' => function ($query) use ($request) {
-                $query->where('branch_id', $request->branch_id);
-            }])
-            ->get()
-            ->groupBy('date')
-            ->map(function ($items, $date) {
-                $evaluatorCount = $items->countBy('evaluator_id');
-                $result = [];
-                foreach ($items as $item) {
-                    $itemData = $item->toArray();
-                    $itemData['evaluator_count'] = $evaluatorCount[$item->evaluator_id];
-                    $result[] = $itemData;
-                }
-                return $result;
-            })
-            ->values()
-            ->toArray();
+{
+    $rates = Rate::with(['rateType' => function ($query) use ($request) {
+            $query->where('branch_id', $request->branch_id);
+        }])
+        ->get()
+        ->groupBy('date')
+        ->map(function ($items, $date) {
+            $evaluatorCount = $items->countBy('evaluator_id');
+            $result = [];
+            foreach ($items as $item) {
+                $itemData = $item->toArray();
+                $itemData['rate_type'] = [$itemData['rate_type']];
+                $itemData['evaluator_count'] = $evaluatorCount[$itemData['evaluator_id']];
+                unset($itemData['evaluator_id']);
+                $result[] = $itemData;
+            }
+            return $result;
+        })
+        ->values()
+        ->flatten()
+        ->toArray();
 
-        return ResponseHelper::success($rates, null, 'rates', 200);
-    }
+    $data = ['data' => $rates];
+
+    return ResponseHelper::success($data, null, 'rates', 200);
+}
 
 }
