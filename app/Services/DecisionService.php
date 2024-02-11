b620e2d59bc0cd2d1b0ca7e039224afda13d4e2d
ali007dev <ali007developer@gmail.com>
2024-02-11 23:42:09 +0300

    a

---
+++ app/Services/DecisionService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Helper\ResponseHelper;
+use Illuminate\Http\Request;
+
+class UserDecisionService
+{
+    public static function user_decisions(Request $request)
+    {
+        $userId = $request->user_id;
+        $date = $request->date;
+        $type = $request->type;
+
+        $year = null;
+        $month = null;
+
+        if (strlen($date) === 4) {
+            $year = $date;
+        } elseif (strlen($date) === 7) {
+            $year = substr($date, 0, 4);
+            $month = substr($date, 5, 2);
+        }
+
+        $result = User::query()
+            ->where('id', $userId)
+            ->with(['my_decisions' => function ($query) use ($year, $month, $type) {
+                if ($year && !$month) {
+                    $query->whereYear('dateTime', $year);
+                } elseif ($year && $month) {
+                    $query->whereYear('dateTime', $year)
+                        ->whereMonth('dateTime', $month);
+                }
+                $query->where('type', $type);
+            }])
+            ->first();
+
+        return ResponseHelper::success($result, null);
+    }
+}
