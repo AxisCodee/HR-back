3a3d221a81d8abc3db5dfa3c66d6b56393573bc1
ali007dev <ali007developer@gmail.com>
2024-02-11 10:15:17 +0300

    aa

--- routes/api.php
+++ routes/api.php
@@ -224,6 +224,9 @@ Route::prefix('Absence')->group(function () {
         Route::get('Uabsences', 'unjustifiedAbsence');
         Route::post('DynamicDecision/{absences}', 'DynamicDecision');
         Route::post('AddAbsence', 'store_absence');
+        Route::get('getUnjustified/{user}', 'getUnjustified');
+
+
     });
 });
 
