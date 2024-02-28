d842910a30679c5729b07d401ccc9825b19ffe04
thalesMworkAcc <thalesmwork942@gmail.com>
2024-02-28 11:56:28 +0300

    fixes

--- database/migrations/2024_01_07_135359_create_attendances_table.php
+++ database/migrations/2024_01_07_135359_create_attendances_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,11 +20,8 @@ return new class extends Migration
             $table->string('status', 255);   // Specify appropriate length
             $table->string('work_code');
             $table->timestamps();
-
-
         });
     }
-
 
 
     /**
