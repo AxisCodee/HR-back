3ff440805bbe5695372c3faee5636c3d51619771
ali007dev <ali007developer@gmail.com>
2024-03-06 11:58:17 +0300

    a

--- app/Jobs/StoreAttendanceLogsJob.php
+++ app/Jobs/StoreAttendanceLogsJob.php
@@ -92,24 +92,28 @@ class StoreAttendanceLogsJob implements ShouldQueue
                         $minutesLate = $parsedHour->diffInMinutes($companyStartTime);
                         $userId = User::query()->where('pin', ($log['PIN']));
                         $lates = Late::query()
-                            ->where('user_id', $userId)
-                            ->whereDate('lateDate', '=', $checkInDate)
-                            ->whereNull('check_in')
-                            ->whereNull('check_out')
-                            ->first();
-                            if (empty($lates)) {
-                                $newLateData = [
-                                    'user_id' => $userId->pin,
-                                    'lateDate' => $checkInDate,
-                                    'check_in' => $log['Status'] == 0 ? $checkInHour : null,
-                                    'check_out' => $log['Status'] == 1 ? $checkOutHour : null,
-                                    'hours_num' => $log['Status'] == 1 ? $hoursOverTime : $hoursLate,
-                                ];
+                        ->where('user_id', $userId->pin)
+                        ->whereDate('lateDate', '=', $checkInDate)
+                        ->whereNull('check_in')
+                        ->whereNull('check_out')
+                        ->first();
 
-                                if ($userId) {
-                                    $newLate = Late::updateOrCreate(['lateDate' => $newLateData['lateDate'],'user_id' => $newLateData['user_id']] ,$newLateData);
-                                }
-                            } else {
+                    if (empty($lates)) {
+                        $newLateData = [
+                            'user_id' => $userId->pin,
+                            'lateDate' => $checkInDate,
+                            'check_in' => $log['Status'] == 0 ? $checkInHour : null,
+                            'check_out' => $log['Status'] == 1 ? $checkOutHour : null,
+                            'hours_num' => $log['Status'] == 1 ? $hoursOverTime : $hoursLate,
+                        ];
+
+                        if ($userId) {
+                            $newLate = Late::updateOrCreate(
+                                ['lateDate' => $newLateData['lateDate'], 'user_id' => $newLateData['user_id']],
+                                $newLateData
+                            );
+                        }
+                    } else {
                                 $lates->update([
                                     'check_in' => $checkInHour,
                                 ]);
