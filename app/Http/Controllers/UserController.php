a5e532cc91e45f4b08597acdd0906bca613b0e39
unknown <mazharalhayek12@gmail.com>
2024-03-06 12:15:29 +0300

    user info with all requests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -53,12 +53,13 @@ class UserController extends Controller
             ->with('department', 'userInfo:id,user_id,image')
             ->whereNull('deleted_at')->get()->toArray();
         $now = Carbon::now();
-        $startTime = Carbon::now()->hour(9)->minute(0)->second(0);
-        foreach ($all_users as $index => &$user) {
+        $startTime =  Carbon::parse('09:00:00');
+        foreach ($all_users as $user) {
             $status = Attendance::query()
-                ->where('pin', $user['pin'])
-                ->whereRaw("STR_TO_DATE(datetime, '%Y-%m-%d %H:%i:%s') BETWEEN ? AND ?", [$startTime, $now])
-                ->value('status');
+                ->where('pin', $user['pin'])->get();
+                // ->whereBetween('datetime',[$startTime , $now])
+                // ->value('status');
+                dd($status);
             $user['status'] = $status;
         }
         return ResponseHelper::success($all_users, null, 'all users info returned successfully', 200);
