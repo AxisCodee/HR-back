9d723a3a77326ae078b6063ffe7eac9b9b352da5
AxisCodee <raneemma934@gmail.com>
2024-04-02 21:01:16 +0300

    a

--- app/Models/User.php
+++ app/Models/User.php
@@ -467,7 +467,7 @@ class User extends Authenticatable implements JWTSubject
                                 ->whereNot('type','justified');
 
              $absences =$this->usertimeService->filterDate($absence, $date, 'startDate')->count();
-                dd($absences);
+                dd($worktime);
             $absencehours = $absences * $worktime;
             $totalhours = $absencehours + $late;
             return $totalhours;}
