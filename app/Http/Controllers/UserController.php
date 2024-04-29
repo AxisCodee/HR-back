235b173f0be08c841aeb7da9ee5be4e7b9677c8a
thalesMworkAcc <thalesmwork942@gmail.com>
2024-04-29 13:19:39 +0300

    fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -60,6 +60,7 @@ class UserController extends Controller
     {
         $all_users = User::query()
             ->where('branch_id', $request->branch_id)
+            ->where('role','employee')
             ->with('department', 'userInfo:id,user_id,image')
             ->whereNull('deleted_at')
             ->get()
