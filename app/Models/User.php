0efd37c5d1955d4fa8c77f340adff7ed48936d22
thalesMworkAcc <thalesmwork942@gmail.com>
2024-04-30 13:45:10 +0300

    fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,7 +63,7 @@ class User extends Authenticatable implements JWTSubject
         'overtime',
         'absence',
         'late',
-        'CheckIPercentage',
+        'CheckInPercentage',
         'CheckOutPercentage',
         'BaseSalary',
         // 'deductions',
