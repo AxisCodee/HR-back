ac074c52c4098f56d5755809352cc95096e853e1
Ziad Bani Almargih <ziadbanialmargih@gmail.com>
2024-06-26 12:34:17 +0300

    fix FingerprintService.php

--- app/Services/FingerprintService.php
+++ app/Services/FingerprintService.php
@@ -376,7 +376,7 @@ class FingerprintService
                 // Check if the parsed check-out hour is after the company end time
                 if ($parsedHour->gt($companyEndTime)) {
                     $diffInMinutes = $parsedHour->diffInMinutes($companyEndTime);
-                    if ($diffInMinutes >= 15) {
+                    if ($diffInMinutes <= -15) {
                         $diffLate = $parsedHour->diff($companyEndTime);
                         $hoursOverTime = $diffLate->format('%H.%I');
                         $this->storeUserOverTime(
