334d94fe77fdbf65a481d73c2f4216e359f7c28f
Thales Mustafa <thales.98mustafa@gmail.com>
2024-06-27 09:05:12 +0300

    fixes

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -78,7 +78,7 @@ class AttendanceController extends Controller
             // $all_user_info = $tad->get_all_user_info();
             // $dt = $tad->get_date();
             $logs = $tad->get_att_log();
-            dd($logs);
+            
             //check date table and store attendance
             $uniqueDates = [];
             //dd(Date::query()->where('branch_id', $branchId)->get()->count() > 0);
@@ -88,6 +88,7 @@ class AttendanceController extends Controller
                 $filtered_att_logs = $logs->filter_by_date(
                     ['start' => $start, 'end' => $end]
                 );
+                dd($filtered_att_logs);
                 $xml = simplexml_load_string($filtered_att_logs);
                 $uniqueDates = $this->fingerprintService->convertAndStoreAttendance($xml, $branchId,$request);
                 $allAttendances = Attendance::query()
