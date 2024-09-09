78a9829c81c1691dd723e8b5651b4ce049a82d72
Ziad Bani Almargih <ziadbanialmargih@gmail.com>
2024-09-09 16:25:41 +0300

    fix reports

--- app/Models/User.php
+++ app/Models/User.php
@@ -304,7 +304,7 @@ class User extends Authenticatable implements JWTSubject
     public function totalLates() //for All lates
     {
         $date = request()->query('date');
-        $result = $this->hasMany(Late::class, 'user_id')->whereNotNull('check_in');
+        $result = $this->hasMany(Late::class, 'user_id')->whereNull('end');
         return $this->usertimeService->filterDate($result, $date, 'lateDate');
     }
 
