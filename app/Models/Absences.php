7314da77da434857588412a25697865b49364d3c
Ziad Bani Almargih <ziadbanialmargih@gmail.com>
2024-09-10 12:38:02 +0300

    translate Absence type

--- app/Models/Absences.php
+++ app/Models/Absences.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -23,8 +24,23 @@ class Absences extends Model
         'demands_compensation'
     ];
 
+    protected $appends = [
+        'type_ar'
+    ];
+
     public function users(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function typeAr(): Attribute
+    {
+        return Attribute::get(function (){
+            return match ($this->type){
+                'justified' => 'مبرر',
+                'Unjustified' => 'غير مبرر',
+                'sick' => 'مريض'
+            };
+        });
     }
 }
